08fb934c79288ecd09406af923a8304acfd7d20d
Artyshook <saibel@email.cz>
2022-12-08 21:21:36 +0100

    mui fix

--- src/App.tsx
+++ src/App.tsx
@@ -36,31 +36,3 @@ function App() {
 }
 
 export default App
-
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#fefefe',
-//     },
-//     secondary: blue,
-//   },
-//   typography: {
-//     fontFamily: 'Lora',
-//     fontWeightLight: 400,
-//     fontWeightRegular: 500,
-//     fontWeightMedium: 600,
-//     fontWeightBold: 700,
-//   },
-// })
-
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     box-sizing: border-box;
-//     background-color: ${theme.background.backgroundColor};
-//     font-family: 'Montserrat', sans-serif;
-//     -ms-overflow-style: none;
-//   }
-//   body::-webkit-scrollbar {
-//     display: none;
-//   }
-// `
--- src/pages/CreateCard.tsx
+++ src/pages/CreateCard.tsx
@@ -1,18 +1,65 @@
-import { Button, Container, Typography } from '@mui/material'
-import { makeStyles } from '@mui/styles'
-import React from 'react'
+import { Button, Container, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+type TaskType = {
+  id: string
+  title: string
+  details: string
+}
 
 export const CreateCard = () => {
-  const styles = makeStyles({})
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [task, setTask] = useState([] as TaskType[])
+
+  const handleClick = () => {
+    const id = '123'
+    setTask([...task, { id, title, details }])
+  }
 
   return (
     <Container>
-      <Typography variant={'h3'} color={'secondary'} align={'center'}>
+      <Typography variant={'h3'} color={'secondary'}>
         Enter question
       </Typography>
-      <Button color={'primary'} variant={'contained'} type={'submit'} size={'large'}>
-        Click
+      <form noValidate autoComplete={'off'}>
+        <MyTextField
+          onChange={e => setTitle(e.currentTarget.value)}
+          label={'No title'}
+          variant={'outlined'}
+          color={'secondary'}
+          fullWidth
+          required
+        />
+        <MyTextField
+          onChange={e => setDetails(e.currentTarget.value)}
+          label={'Details'}
+          variant={'outlined'}
+          color={'secondary'}
+          multiline
+          rows={4}
+          fullWidth
+          required
+        />
+      </form>
+      <Button
+        onClick={handleClick}
+        color={'secondary'}
+        variant={'contained'}
+        type={'submit'}
+        size={'large'}
+      >
+        Add
       </Button>
     </Container>
   )
 }
+
+const MyTextField = styled(TextField)`
+  && {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: block;
+  }
+`
