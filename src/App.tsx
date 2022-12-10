a90f59d2f810d838959bf523614611ba4da51835
Artyshook <saibel@email.cz>
2022-12-10 17:55:07 +0100

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,55 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateCard } from './pages/CreateCard'
 import { ThemeProvider } from '@mui/material'
 import { blue } from '@mui/material/colors'
+import { createGlobalStyle } from 'styled-components'
 import { createTheme } from '@mui/material/styles'
+import { theme } from './helpers/theme'
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#fefefe',
-    },
-    secondary: blue,
-  },
-  typography: {
-    fontFamily: 'Lora',
-    fontWeightLight: 400,
-    fontWeightRegular: 500,
-    fontWeightMedium: 600,
-    fontWeightBold: 700,
-  },
-})
+// export const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#fefefe',
+//     },
+//     secondary: blue,
+//   },
+//   typography: {
+//     fontFamily: 'Lora',
+//     fontWeightLight: 400,
+//     fontWeightRegular: 500,
+//     fontWeightMedium: 600,
+//     fontWeightBold: 700,
+//   },
+// })
 
 function App() {
   return (
     <div>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<CreateCard />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      {/*<ThemeProvider theme={theme}>*/}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<CreateCard />} />
+        </Routes>
+      </BrowserRouter>
+      <GlobalStyle />
+      {/*</ThemeProvider>*/}
     </div>
   )
 }
 
 export default App
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    box-sizing: border-box;
+    background-color: ${theme.background.backgroundColor};
+    font-family: 'Montserrat', sans-serif;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+  body::-webkit-scrollbar {
+    display: none;
+  }
+  html {
+    font-size: 85%;
+  }
+`
--- src/pages/CreateCard.tsx
+++ src/pages/CreateCard.tsx
@@ -1,65 +1,142 @@
-import { Button, Container, TextField, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  Typography,
+} from '@mui/material'
+import { useLocalStorage } from '../helpers/functions'
+import { useNavigate } from 'react-router-dom'
+import { v1 } from 'uuid'
+import React, { FormEvent, useState } from 'react'
 import styled from 'styled-components'
 
 type TaskType = {
   id: string
   title: string
   details: string
+  category: string
 }
 
 export const CreateCard = () => {
   const [title, setTitle] = useState('')
   const [details, setDetails] = useState('')
-  const [task, setTask] = useState([] as TaskType[])
+  const [category, setCategory] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+  const [task, setTask] = useLocalStorage('task', [] as TaskType[])
+  const navigate = useNavigate()
 
-  const handleClick = () => {
-    const id = '123'
-    setTask([...task, { id, title, details }])
+  console.log(task)
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title === '') {
+      setTitleError(true)
+    }
+    if (details === '') {
+      setDetailsError(true)
+    } else if (title && details) {
+      setTask([...task, { id: v1(), title, details, category }])
+      navigate('/home')
+    }
   }
 
   return (
-    <Container>
-      <Typography variant={'h3'} color={'secondary'}>
-        Enter question
-      </Typography>
-      <form noValidate autoComplete={'off'}>
-        <MyTextField
-          onChange={e => setTitle(e.currentTarget.value)}
-          label={'No title'}
-          variant={'outlined'}
-          color={'secondary'}
-          fullWidth
-          required
-        />
-        <MyTextField
-          onChange={e => setDetails(e.currentTarget.value)}
-          label={'Details'}
-          variant={'outlined'}
-          color={'secondary'}
-          multiline
-          rows={4}
-          fullWidth
-          required
-        />
-      </form>
-      <Button
-        onClick={handleClick}
-        color={'secondary'}
-        variant={'contained'}
-        type={'submit'}
-        size={'large'}
+    <Box_Wrapper
+      component={'main'}
+      flexDirection={{ xs: 'column', md: 'row' }}
+      minHeight={'calc(100vh - 175px)'}
+    >
+      <Box
+        display={'flex'}
+        gap={'1.5rem'}
+        justifyContent={'center'}
+        flexDirection={'column'}
+        width={'60%'}
       >
-        Add
-      </Button>
-    </Container>
+        <Typography variant={'h3'} color={'secondary'}>
+          Enter question
+        </Typography>
+        <form noValidate autoComplete={'off'} onSubmit={handleSubmit}>
+          <MyTextField
+            onChange={e => setTitle(e.currentTarget.value)}
+            label={'No title'}
+            variant={'outlined'}
+            color={'secondary'}
+            fullWidth
+            required
+            error={titleError}
+          />
+          <MyTextField
+            onChange={e => setDetails(e.currentTarget.value)}
+            label={'Details'}
+            variant={'outlined'}
+            color={'secondary'}
+            multiline
+            rows={4}
+            fullWidth
+            required
+            error={detailsError}
+          />
+          <MyFormControl>
+            <FormLabel>Category</FormLabel>
+            <RadioGroup value={category} onChange={e => setCategory(e.currentTarget.value)}>
+              <FormControlLabel control={<Radio color={'secondary'} />} label={'1'} value={'1'} />
+              <FormControlLabel control={<Radio color={'secondary'} />} label={'2'} value={'2'} />
+              <FormControlLabel control={<Radio color={'secondary'} />} label={'3'} value={'3'} />
+            </RadioGroup>
+          </MyFormControl>
+
+          <Button color={'secondary'} variant={'contained'} type={'submit'} size={'large'}>
+            Add
+          </Button>
+        </form>
+      </Box>
+    </Box_Wrapper>
   )
 }
+
+const MyContainer = styled(Container)`
+  && {
+    max-width: 1140px;
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`
 
 const MyTextField = styled(TextField)`
   && {
     margin-top: 20px;
     margin-bottom: 20px;
     display: block;
+  }
+`
+const MyFormControl = styled(FormControl)`
+  && {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: block;
+  }
+`
+const Box_Wrapper = styled(Box)`
+  && {
+    padding-top: 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
 `
